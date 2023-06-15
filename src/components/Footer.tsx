286bb31ffac0a9bd4cc00f35bc193a8592ce3418
hubiwibe <hubiwibe@gmail.com>
2023-06-15 19:14:44 +0900

    🌱 Add: 푸터 컴포넌트 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="py-2 text-sm text-center text-white bg-black">
+      {"Don't forget to CODE your DREAM | All Right Reserved."}
+    </footer>
+  );
+}
