68cf97b378a2cab49c0cf7d22d756c22bf07ae69
hubiwibe <hubiwibe@gmail.com>
2023-06-15 19:14:33 +0900

    🌱 Add: 헤더 컴포넌트 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between h-20 px-5">
+      <Link className="text-2xl font-bold" href="/">
+        {"Hubiwibe's Blog"}
+      </Link>
+      <nav className="flex gap-x-5">
+        <Link href="/">home</Link>
+        <Link href="/about">about</Link>
+        <Link href="/posts">posts</Link>
+        <Link href="/contact">contact</Link>
+      </nav>
+    </header>
+  );
+}
