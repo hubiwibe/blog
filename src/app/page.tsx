c21a57856f53c9658e1e19386d6f71c76722df03
Yungi Jung <hubiwibe@gmail.com>
2024-01-10 22:06:56 +0900

    🌱 Add: 홈 경로에 포스트 카드 목록

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import PostCards from '@/components/PostCards';
 import Profile from '@/components/Profile';
 
 export default function HomePage() {
   return (
-    <section>
+    <section className="py-5">
       <Profile />
+      <div className="h-8" />
+      <PostCards />
     </section>
   );
 }
