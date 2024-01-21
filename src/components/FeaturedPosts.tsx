2c6c33fcd7d411236e415c4d13770251d5a2c3ec
Yungi Jung <hubiwibe@gmail.com>
2024-01-21 15:44:46 +0900

    💄 Add: Featured posts title

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -8,6 +8,7 @@ async function FeaturedPosts() {
   // 2. 모든 데이터를 보여줌
   return (
     <section>
+      <h2 className="mb-1 text-2xl font-bold">Featured Posts</h2>
       <PostCards posts={posts} />
     </section>
   );
