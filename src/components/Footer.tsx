1eaf1e25534345fcec2fe4172c74db6542527668
Yungi Jung <hubiwibe@gmail.com>
2024-01-22 20:24:58 +0900

    💬 Update: Footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function Footer() {
-  return (
-    <footer className="py-2 text-sm text-center text-white bg-black">
-      {"Don't forget to CODE your DREAM | All Right Reserved."}
-    </footer>
-  );
+  return <footer className="py-2 text-sm text-center text-white bg-black">{'All Right Reserved.'}</footer>;
 }
