090a1e0e4f91ea488d5b23f96e2afed8cc3be81a
Yungi Jung <hubiwibe@gmail.com>
2024-01-25 21:32:45 +0900

    💬 Update: Header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between h-20 px-5">
       <Link className="text-2xl font-bold" href="/">
-        {"Justin's Blog"}
+        사탕공방
       </Link>
       <nav className="flex gap-x-5">
         <Link href="/">home</Link>
