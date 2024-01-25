17621746200d8fcc43977471e424cfb2ffd5f8a8
Yungi Jung <hubiwibe@gmail.com>
2024-01-25 21:33:52 +0900

    🌱 Add: Apply posts page template

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,14 @@
+import PostsTemplate from '@/components/PostsTemplate';
+import { getAllPosts } from '@/service/posts';
 import React from 'react';
 
-export default function PostsPage() {
-  return <section>PostsPage</section>;
+export default async function PostsPage() {
+  const posts = await getAllPosts();
+  const categories = new Set(posts?.map(({ category }) => category));
+
+  return (
+    <div className="flex pb-4">
+      <PostsTemplate posts={posts} categories={['All posts', ...categories.values()]} />
+    </div>
+  );
 }
