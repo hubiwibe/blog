3dabb63fa454d50df44b2a509f11214f85ed7b17
Yungi Jung <hubiwibe@gmail.com>
2024-03-19 22:05:19 +0900

    style: Update post detail prev and next navigation

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -21,13 +21,57 @@ async function PostDetailPage({ params: { id } }: { params: { id: string } }) {
       <PostContent post={post} />
       <section className="flex justify-between">
         {prev && (
-          <Link className="w-full bg-red-400" href={`${prev.path}`}>
-            이전 포스트
+          <Link className="relative flex w-full h-56" href={`${prev.path}`}>
+            <Image
+              className="absolute object-cover w-full h-full"
+              src={`/images/posts/${prev.path}.png`}
+              alt={title}
+              width={760}
+              height={420}
+            />
+            <div className={`z-10 flex items-center justify-around ${next ? 'w-full' : 'w-3/4'}`}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={4}
+                stroke="currentColor"
+                className="w-12 h-12 text-yellow-400"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+              </svg>
+              <div className="flex flex-col">
+                <span className="text-3xl font-extrabold text-white">{prev.title}</span>
+                <span className="font-bold text-white">{prev.description}</span>
+              </div>
+            </div>
           </Link>
         )}
         {next && (
-          <Link className="w-full bg-green-400" href={`${next.path}`}>
-            다음 포스트
+          <Link className={`relative flex w-full h-56${prev ? '' : ' justify-end'}`} href={`${next.path}`}>
+            <Image
+              className="absolute object-cover w-full h-full"
+              src={`/images/posts/${next.path}.png`}
+              alt={title}
+              width={760}
+              height={420}
+            />
+            <div className={`z-10 flex items-center justify-around ${prev ? 'w-full' : 'w-3/4'}`}>
+              <div className="flex flex-col">
+                <span className="text-3xl font-extrabold text-white">{next.title}</span>
+                <span className="font-bold text-white">{next.description}</span>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={4}
+                stroke="currentColor"
+                className="w-12 h-12 text-yellow-400"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+              </svg>
+            </div>
           </Link>
         )}
       </section>
